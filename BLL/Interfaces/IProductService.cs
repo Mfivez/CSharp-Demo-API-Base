4df3cb7009e2060687c3d6c2b07f956346fecc7e
mauritcio <mauritcio.fivez@bstorm.be>
2026-04-16 09:23:05 +0200

    fix: achange getProductByIdAsync return type from Product? to Product

--- BLL/Interfaces/IProductService.cs
+++ BLL/Interfaces/IProductService.cs
@@ -8,7 +8,7 @@ namespace BLL.Interfaces
     public interface IProductService
     {
         Task<List<Product>> GetAllProductsAsync();
-        Task<Product?> GetProductByIdAsync(int id);
+        Task<Product> GetProductByIdAsync(int id);
         Task CreateProductAsync(Product product);
         Task UpdateProductAsync(Product product);
         Task DeleteProductAsync(int id);
